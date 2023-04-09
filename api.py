106941c2a7531598025660b9183630a73a8623c0
Hamza <hhumayun@gmail.com>
2023-04-09 16:28:34 +0000

    Commit before

--- api.py
+++ api.py
@@ -1,13 +1,43 @@
-from fastapi import Depends, FastAPI
 
-from app.db import User, create_db_and_tables
+from fastapi import Depends, FastAPI, HTTPException
+
+from fastapi_users import exceptions, models
+from fastapi_users.manager import BaseUserManager, UserManagerDependency
+
+import contextlib
+from sqlalchemy import insert
+
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+
+from pydantic import BaseModel
+
+from fastapi import status
+
+import requests
+from fastapi.security import OAuth2PasswordRequestForm
+from app.db import User, VideoReq, create_db_and_tables, get_async_session
 from app.schemas import UserCreate, UserRead, UserUpdate
-from app.users import auth_backend, current_active_user, fastapi_users
+from app.users import auth_backend, current_active_user, fastapi_users, get_user_db, get_user_manager
+from summarize import summarize
+
+
+class AuthPassChange(BaseModel):
+    email: str
+    password: str
+    new_password: str
+
+
+class Auth(BaseModel):
+    username: str
+    password: str
+
 
 app = FastAPI()
 
 app.include_router(
-    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
+    fastapi_users.get_auth_router(auth_backend, requires_verification=True), prefix="/auth/jwt", tags=["auth"]
+
 )
 app.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
@@ -41,51 +71,63 @@ async def summary(video_id):
     return summarize(video_id=video_id)
 
 
-# @app.route('/register', methods=['POST'])
-# def register():
-#     email = request.form['email']
-#     password = request.form['password']
+# @app.get('/auth/verify2/{token}')
+# async def verify2(token: str):
+#    url = 'http://localhost:8000/auth/verify'
+#    myobj = {'token': token}
 
-#     cur = conn.cursor()
-#     cur.execute("INSERT INTO users (email, password, is_active) VALUES (%s, %s, %s) RETURNING id",
-#                 (email, password, False))
-#     user_id = cur.fetchone()[0]
-#     conn.commit()
-#     cur.close()
+#    x = await requests.post(url, json=myobj)
 
-#     token = generate_confirmation_token(email)
-#     confirm_url = f'http://localhost:5000/confirm/{token}'
-#     message = Message('Confirm your account', recipients=[email])
-#     message.body = f'Thank you for registering. Please confirm your account by clicking the link below:\n\n{confirm_url}'
-#     mail.send(message)
-
-#     return jsonify({'message': 'Registration successful. Please check your email for confirmation instructions.'}), 201
+#    if x.status_code == 200:
+#        return "Account verified"
+#    else:
+#        return "Error - Invalid token "
 
 
-# @app.route('/confirm/<token>', methods=['GET'])
-# def confirm(token):
-#     email = confirm_token(token)
-#     if email is None:
-#         return jsonify({'message': 'The confirmation link is invalid or has expired.'}), 400
+@app.post('/auth/changepassword/')
+async def password_change(pass_change: AuthPassChange, user_manager: BaseUserManager[models.UP, models.ID] = Depends(get_user_manager)):
+    #user_db = get_user_db(get_async_session())
+    auth = Auth(username=pass_change.email, password=pass_change.password)
 
-#     cur = conn.cursor()
-#     cur.execute("UPDATE users SET is_active = %s WHERE email = %s",
-#                 (True, email))
-#     conn.commit()
-#     cur.close()
+    valid_user = await user_manager.authenticate(auth)
 
-#     return jsonify({'message': 'Account confirmed. You can now login.'}), 200
+    if not valid_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid username or password")
+
+    updated_user = await user_manager.reset_password_user(valid_user, pass_change.new_password)
+    print(updated_user)
+
+    return {"message": f"Password updated successfully "}
+
+    # make this async
 
 
-# def generate_confirmation_token(email):
-#     s = Serializer(app.config['SECRET_KEY'], expires_in=3600)
-#     return s.dumps({'email': email}).decode('utf-8')
+@app.get("/authenticated-route")
+def authenticated_route(user: User = Depends(current_active_user)):
+    return {"message": f"Hello {user.email}!"}
 
 
-# def confirm_token(token, expiration=3600):
-#     s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
-#     try:
-#         data = s.loads(token.encode('utf-8'))
-#     except:
-#         return None
-#     return data.get('email')
+@ app.get("/summarize/{video_id}")
+def summary_(video_id: str, user: User = Depends(current_active_user)):
+
+    DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost/postgres"
+    engine = create_async_engine(DATABASE_URL)
+    session = async_sessionmaker(engine, expire_on_commit=False)
+    d = session.begin()
+
+    summary = summarize(video_id=video_id)
+
+    stmt = insert(VideoReq).values(email=user.email,
+                                   videoid=video_id, summary=str(summary))
+
+    d.async_session.execute(stmt)
+
+    return summary
+
+
+@ app.on_event("startup")
+async def on_startup():
+    # Not needed if you setup a migration system like Alembic
+    await create_db_and_tables()
