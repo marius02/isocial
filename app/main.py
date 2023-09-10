cefd986c153ae386736494987d30ea4b2db2329d
Aleksei Mikhalev <aleks_mihalev@list.ru>
2023-09-10 16:34:39 +0300

    revise CORS policy

--- app/main.py
+++ app/main.py
@@ -27,8 +27,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['GET', 'POST', 'OPTIONS', 'DELETE', 'PATCH', 'PUT'],
+    allow_headers=['Content-Type', 'Set-Cookie', 'Access-Control-Allow-Headers',
+                   'Access-Control-Allow-Origin', 'Authorization'],
 )
 
 app.include_router(chat_router)
