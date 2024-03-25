c12c72d9bf37583b79118ba921259b4829a9f123
Aleksei Mikhalev <aleks_mihalev@list.ru>
2024-03-25 12:33:04 +0400

    add https middleware

--- app/main.py
+++ app/main.py
@@ -9,23 +9,24 @@ from app.api.users.services import auth_backend, fastapi_users
 from app.db.db_config import create_db_and_tables
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 
-app = FastAPI(title='iSocial')
+app = FastAPI(title="iSocial")
 
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-ssl_context.load_cert_chain('/path/to/cert.pem', keyfile='/path/to/key.pem')
+ssl_context.load_cert_chain("/path/to/cert.pem", keyfile="/path/to/key.pem")
 
 origins = [
-    'http://23.100.16.133',
-    'http://23.100.16.133:8000',
-    'https://23.100.16.133',
-    'https://23.100.16.133:8000',
-    'https://isocial.ai'
+    "http://23.100.16.133",
+    "http://23.100.16.133:8000",
+    "https://23.100.16.133",
+    "https://23.100.16.133:8000",
+    "https://isocial.ai",
 ]
 
 # Allow these methods to be used
-methods = ['GET', 'POST', 'OPTIONS', 'DELETE', 'PATCH', 'PUT']
+methods = ["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"]
 
 # Only these headers are allowed
 headers = ["Content-Type", "Accept", "Authorization"]
@@ -36,32 +37,47 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=methods,
-    allow_headers=headers
+    allow_headers=headers,
 )
+app.add_middleware(HTTPSRedirectMiddleware)
 
 app.include_router(chat_router)
-app.include_router(fastapi_users.get_auth_router(auth_backend,
-                                                 requires_verification=True),
-                   prefix="/auth/jwt", tags=["Authentication and authorization  "])
-app.include_router(fastapi_users.get_register_router(UserRead, UserCreate),
-                   prefix="/auth",
-                   tags=["Authentication and authorization"])
-app.include_router(fastapi_users.get_reset_password_router(),
-                   prefix="/auth",
-                   tags=["Authentication and authorization"])
-app.include_router(fastapi_users.get_verify_router(UserRead),
-                   prefix="/auth",
-                   tags=["Authentication and authorization"])
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend, requires_verification=True),
+    prefix="/auth/jwt",
+    tags=["Authentication and authorization  "],
+)
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["Authentication and authorization"],
+)
+app.include_router(
+    fastapi_users.get_reset_password_router(),
+    prefix="/auth",
+    tags=["Authentication and authorization"],
+)
+app.include_router(
+    fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+    tags=["Authentication and authorization"],
+)
 app.include_router(user_router)
-app.include_router(fastapi_users.get_users_router(UserRead, UserUpdate),
-                   prefix="/users",
-                   tags=["Users"])
-app.include_router(notification_router,
-                   prefix="/auth",
-                   tags=["Authentication and authorization"],)
-app.include_router(payment_router,
-                   prefix="/auth",
-                   tags=["Authentication and authorization"],)
+app.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["Users"],
+)
+app.include_router(
+    notification_router,
+    prefix="/auth",
+    tags=["Authentication and authorization"],
+)
+app.include_router(
+    payment_router,
+    prefix="/auth",
+    tags=["Authentication and authorization"],
+)
 
 
 @app.on_event("startup")
